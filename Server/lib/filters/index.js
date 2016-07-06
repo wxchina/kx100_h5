bdfc75c94a44e52af29a1d19a91814d53ae675e9
suna5511 <jian_suna@163.com>
2016-07-06 09:28:01 +0800

    S

---
+++ Server/lib/filters/index.js
@@ -0,0 +1,26 @@
+/**
+ * Author:      changyingwei
+ * Create Date: 2014-12-01  
+ * Description: 前端过滤器
+ */
+module.exports.filterRegister = function(express, config) {
+    'use strict';
+
+    var router = express.Router(), //自定义过滤器
+        request = require('request'),
+        serveIndex = require('serve-index');
+
+    //首页和登录页路由
+    router.get('/', function(req, res) {
+        if (req.authorized) {
+            res.render(config.homepage);
+        } else {
+            res.render(config.loginpage);
+        }
+    });
+    //测试路由
+    router.get('/demo|/demo/*', serveIndex(config.demopath, {
+        'icons': true
+    }));
+    return router;
+};
---
+++ Server/tasks/task.perf.js
@@ -0,0 +1,8 @@
+/**
+ * Author:      changyingwei
+ * Create Date: 2015-07-07  
+ * Description: 自动化监控前端性能
+ */
+module.exports = function() {
+
+};
